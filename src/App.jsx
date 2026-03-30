0590d11a653f020db74363bb7d928cf547630761
John Hunter <johnroberthunter1@gmail.com>
2026-03-30 07:54:20 -0600

    blank slate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import { MdFace3 } from "react-icons/md";
 
 function App() {
   return (
     <>
-      <div></div>
+      <div>
+        <h1>
+          Hello World <MdFace3 />
+        </h1>
+      </div>
     </>
   );
 }
