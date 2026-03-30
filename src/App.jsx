95845668a5088d8a2e44bb11b25e063b08c546ca
John Hunter <johnroberthunter1@gmail.com>
2026-03-30 08:10:39 -0600

    blank

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from "react";
 import "./App.css";
-import { MdFace3 } from "react-icons/md";
 
 function App() {
   return (
     <>
       <div>
-        <h1>
-          Hello World <MdFace3 />
-        </h1>
+        <h1></h1>
       </div>
     </>
   );
